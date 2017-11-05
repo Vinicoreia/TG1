66982ab8d385004f3b5fa262a947185878a96907
Vinicoreia <shadowtrolling@gmail.com>
2017-11-05 18:25:23 -0200

    apagando arquivos lixo e criando pasta de testfile

--- LZ77.cpp
+++ LZ77.cpp
@@ -1,293 +1,322 @@
 #include <bits/stdc++.h>
-#include <fstream>
-#define BUFFERSIZE 4096
-#define DICTSIZE 2047
-#define LOOKAHEADSIZE 255
+#define DICTSIZE 16383
+#define LOOKAHEADSIZE 15
 #define WINDOWSIZE LOOKAHEADSIZE + DICTSIZE
-// This is a simple implementation of the lempel-ziv algorithm
+#define DICTBITS 14
+#define LOOKBITS 4
 using namespace std;
 
 typedef struct code
 {
-    string foundString;
     size_t offset;
+    string foundString;
     char nextChar;
 } code;
 
-
-
-deque<char> lookahead;
-deque<char> dict;
-string buffer;
-deque<tuple<size_t, size_t, char>> codigo;
-int windowPointer = 0; /*aponta para o final do lookahead*/
-int dictPointer = 0; /*aponta pro indice no buffer do inicio do dicionario*/
-int lookaheadPointer=0; /*aponta pro indice no buffer do inicio do lookahead*/
-int filesize = 0;
-int bufferCount = 0;
-int bufferPointer = 0;
-int numPop = 0;
-/*offset, length, char*/
-tuple<size_t, size_t, char> pegaMaiorSubstring(); /*Deve ser alterada pra retornar 1bit de flag 4 bits de deslocamento 3 bits de tamanho*/
-
-void advanceWindow(int comprimento = 1);
-int getNextBuffer(fstream &file);
-void getFileSize(fstream &file);
+int filesize;
+size_t windowPointer = 0;    /*points to end of window*/
+size_t dictPointer = 0;      /*points to beg of dict*/
+size_t lookaheadPointer = 0; /*points to beg of lookahead*/
+string window;
+string fileBuffer;
+string lookahead;
+string dict;
+string binaryBuffer;
+string bitString;
 string charToBin(char c);
-
-void printVector(deque<char> path)
-{
-    for (auto i = path.begin(); i != path.end(); ++i)
-        std::cout << *i;
-    cout<<endl;
-}
-
-void printPointers(){
-    cout<<"DP: "<<dictPointer<<" LP: "<<lookaheadPointer<<" WP: "<<windowPointer<<" BP: "<<bufferPointer<<endl;
-}
-void printVectors(){
-    cout<<"DICT: ";
-    printVector(dict);
-    cout<<"LOOKAHEAD: ";
-    printVector(lookahead);
-}
+string readFileToBuffer(ifstream &in);
+void getFileSize(ifstream &file);
+void fillBuffer(fstream &file);
+void CompressFile(ifstream &file);
+void getNextWindow(size_t matchSize=0, size_t jump=0);
+code getBiggestSubstring();
+void decompressFile(ifstream &file);
+void startWindow();
+string fileNameIn;
+string fileNameOut;
 
 int main(int argc, char *argv[])
 {
-    fstream file("teste.txt", ios::binary | ios::in | ios::out); // lida com o arquivo, abre em modo binario
-    if (file.is_open())
+    string fileMode;
+    cout<< "The format is <compress|decompress filenameIn filenameOut>"<<endl;
+    if(argc==4){
+        fileNameOut = argv[3];
+        fileNameIn = argv[2];
+        fileMode = argv[1];
+        ifstream file(fileNameIn, ios::in | ios::binary | ios::ate);
+
+        if (file.is_open())
+        {
+            getFileSize(file);
+            fileBuffer = readFileToBuffer(file);
+            if (fileMode == "compress")
+            {
+                CompressFile(file);
+            }
+            else if (fileMode == "decompress")
+            {
+                decompressFile(file);
+            }
+            else
+            {
+                cout << "No option named " << fileMode << endl;
+            }
+            file.close();
+        }
+    }else{
+        cout<<"Wrong format"<<endl;
+    }
+
+    
+        return 0;
+}
+
+
+void decompressFile(ifstream &file)
+{
+    while(fileBuffer.size()>0){
+        bitString.append(charToBin(fileBuffer.at(0)));
+        fileBuffer.erase(0,1);
+    }
+    string bitChar;
+    string lookaheadBits;
+    string dictBits;
+    string outString;
+    int jump;
+    int len;
+    char nextChar;
+    while (bitString.size() > 8)
     {
-        getFileSize(file); // pega o tamanho do arquivo
-        cout << "reading binary file to buffer\n\n";
-        string procura;
-        tuple<size_t, size_t, char> tuplaElementos;
-        while (getNextBuffer(file))
-        {    
-              while (dictPointer<bufferPointer)
-              {   
-                tuplaElementos = pegaMaiorSubstring();
-                if (dictPointer >= buffer.size())
-                {
-                    break;
-                }
-                codigo.push_back(tuplaElementos);
-                advanceWindow(get<1>(tuplaElementos) + 1);
+        if (bitString[0] == '0')
+        {
+            /*
+            * Se o bit tiver a flag 0, le os proximos 8 e adiciona na stringe move a window
+            */
+            bitString.erase(0,1);
+            bitChar = bitString.substr(0, 8);
+            bitString.erase(0,8);
+            nextChar = static_cast<char>(std::stoi(bitChar, 0, 2));
+            
+            outString += nextChar;
+
+            dict += nextChar;
+            windowPointer +=1; 
+            if(dict.size() > DICTSIZE){
+                dict.erase(0,1);
+            }
+        }
+        else
+        {
+            /*
+            * BIT 1: pula jump pra tras e copia len bits do dicionario
+            */
+            bitString.erase(0,1);
+            dictBits = bitString.substr(0, DICTBITS);
+            lookaheadBits = bitString.substr(DICTBITS, LOOKBITS);
+            bitChar = bitString.substr(DICTBITS + LOOKBITS, 8);
+            bitString.erase(0, DICTBITS + LOOKBITS + 8);
+            nextChar = static_cast<char>(std::stoi(bitChar, 0, 2));
+            jump = stoi(dictBits, 0, 2);
+            len = stoi(lookaheadBits, 0, 2);
+            /*COPIAR len bits*/
+            for(int i=0; i<len; i++){
+                outString += dict[(dict.size()-jump+i)%dict.size()];
+            }
+            outString += nextChar;
+            dict.append(outString.substr(windowPointer, len+1));
+            windowPointer+=len+1;
+            if(dict.size()>DICTSIZE){
+                dict.erase(0,len+1);
             }
         }
     }
-    file.close();
-    // fstream newFile("encode.txt", ios::binary | ios::in | ios::out); // lida com o arquivo, abre em modo binario
-    // if (newFile.is_open()){
-        size_t lookaheadBits = 3 ;
-        size_t dictBits = 6;
-        int jump;
-        int len;
-        char next;
-        char primeiro = get<2>(codigo[0]);
-        // newFile.write(primeiro);
-        codigo.pop_front();
-        string bitString;
-        bitString.clear();
-        while(codigo.size()>0){
-            jump = get<0>(codigo[0]);
-            len = get<1>(codigo[0]);
-            next = get<2>(codigo[0]);
-            codigo.pop_front();
-            if(jump == 0){
-                bitString+="0";
-                bitString.append(charToBin(next));
-            }
-            else{
-                bitString+="1"; /*FLAG*/
-                bitString.append(bitset<11>(jump).to_string());
-                bitString.append(bitset<8>(len).to_string());
-                bitString.append(charToBin(next));
-            }
-        }
-    // }
-        cout<<filesize<<endl;
-        cout<<bitString;
-        return 0;
+    ofstream output(fileNameOut, ios::out| ios::binary);
+    output<<outString;
+    output.close();
 }
-void getFileSize(fstream &file)
-{   file.clear();
-    file.seekg(0, ios::beg);
-    file.seekg(0, ios::end);
+
+string charToBin(char c)
+{
+    string charBin;
+    charBin.clear();
+    for (int i = 7; i >= 0; --i)
+    {
+        (c & (1 << i)) ? charBin += '1' : charBin += '0';
+    }
+    return charBin;
+}
+
+string readFileToBuffer(ifstream &fileIn)
+{
+    return static_cast<stringstream const &>(stringstream() << fileIn.rdbuf()).str();
+}
+
+void getFileSize(ifstream &file)
+{
     filesize = file.tellg();
     file.seekg(0, ios::beg);
     file.clear();
 }
-void advanceWindow(int comprimento)
-{   
-    dictPointer+=comprimento;
-    dict.clear();
-    lookahead.clear();
-    if (dictPointer<buffer.size()){
-
-        for (int i = lookaheadPointer, j = 0; j < LOOKAHEADSIZE and i < buffer.size(); i++, j++)
-        {
-            lookahead.push_back(buffer.at(i));
-        }
-        for (int i = lookaheadPointer - 1, j = 0; j < DICTSIZE and i >= 0; i--, j++)
-        {
-            dict.push_front(buffer.at(i));
-        }
+void startWindow(){
+        lookahead = fileBuffer.substr(1, LOOKAHEADSIZE);
+        lookaheadPointer += 1;
+        dict = fileBuffer.substr(0, 1);
+        windowPointer += lookahead.size() + 1;
+}
+void getNextWindow(size_t matchSize, size_t jump){
+    /*
+    *   This function slides the window to create the binary file
+    *
+    */
+    if (jump == 0)
+    {
+        matchSize = 0;
+    }
+    
+    matchSize++;
+    lookaheadPointer += matchSize;
+    windowPointer += matchSize;
+    dictPointer += matchSize;
+    
+    dict.append(fileBuffer.begin() + lookaheadPointer - matchSize, fileBuffer.begin() + lookaheadPointer);
+    lookahead.append(fileBuffer.begin() + windowPointer - matchSize, fileBuffer.begin() + windowPointer);
+    if(dict.size()>DICTSIZE){
+        dict.erase(0, matchSize);
+    }
+    if(lookahead.size()>LOOKAHEADSIZE or lookahead.size()>=filesize){
+        lookahead.erase(0, matchSize);
+    }
+    if (lookaheadPointer>=filesize){
+        lookaheadPointer = filesize;
+        windowPointer=filesize;
+        lookahead.clear();
     }
 }
 
-string charToBin(char c)
-{   
-    string charBin;
-    charBin.clear();
-        for (int i = 7; i >= 0; --i)
-        {
-            (c & (1 << i)) ? charBin += '1' : charBin += '0';
-        }
-        return charBin;
-}
 
-void fillBuffer(fstream &file)
-{   
-    file.seekg(0,ios::beg);
-    file.seekp(bufferCount*BUFFERSIZE);
-    int filePosition = file.tellg();
-    if(filesize-filePosition>0){
-        if (filesize-filePosition>0 and filesize-filePosition<BUFFERSIZE)
-        {   
-            buffer.resize(filesize-filePosition);              /* Coloca 2048 bytes na memoria*/
-            file.read(&buffer[0], buffer.size()); /*Le os bytes*/
-            bufferPointer = buffer.size();
-            
+void CompressFile(ifstream &file){
+    code elementTuple;
+    deque<code> codeTriples;
+    int jump;
+    int len;
+    int max = 0;
+    char next;
+    string matchString;
+    string maxString;
+
+    codeTriples.push_back({0, fileBuffer.substr(0,1),fileBuffer[0]}); /*First item is always present*/
+
+    startWindow();
+
+    while(!lookahead.empty()){
+        elementTuple = getBiggestSubstring();
+        codeTriples.push_back(elementTuple);
+        getNextWindow(elementTuple.foundString.length(), elementTuple.offset);
+    }
+    fileBuffer.clear();
+    
+    while (codeTriples.size() > 0)
+    {
+        jump = codeTriples[0].offset;
+        len = codeTriples[0].foundString.length();
+        matchString = codeTriples[0].foundString;
+        next = codeTriples[0].nextChar;
+        // cout << jump << " " << len << " " << next << endl;
+        if (jump == 0)
+        {
+            bitString += "0";
+            bitString.append(charToBin(next));
+        }
+        else if((len*9 + 8) < (1+DICTBITS+LOOKBITS+8) and jump>0) /*times 9 because we have to apend 'len' bits of flag*/
+        {
+            for (int i = 0; i < len; i++)
+            {
+                
+                bitString += "0"; /*FLAG*/
+                bitString.append(charToBin(codeTriples[0].foundString[i]));
+            }
+            bitString += "0"; /*FLAG*/
+            bitString.append(charToBin(next));
         }
         else
         {
-            
-            buffer.resize(BUFFERSIZE);         /* Coloca 2048 bytes na memoria*/
-            file.read(&buffer[0], BUFFERSIZE); /*Le os bytes*/
-            bufferPointer = BUFFERSIZE;
+             cout<<jump<<" "<<len<<" "<<codeTriples[0].foundString<<endl<<codeTriples[0].nextChar;
+            bitString += "1"; /*FLAG*/
+            bitString.append(bitset<DICTBITS>(jump).to_string());
+            bitString.append(bitset<LOOKBITS>(len).to_string());
+            bitString.append(charToBin(next));
         }
+        codeTriples.pop_front();
     }
-}
-void clearStreams()
-{
-    dictPointer = 0;
-    lookaheadPointer = 0;
-    windowPointer=0;
-    buffer.clear();
-    lookahead.clear();
-    dict.clear();
+    cout<<bitString;
+    
+    while(bitString.size()%8!=0){
+        bitString+="0";
+    }
+    cout<<endl<<endl<<bitString.size()/8;
+    
+    ofstream output(fileNameOut, ios::out | ios::binary);
+    unsigned long c;
+    while(!bitString.empty()){
+        bitset<8> b(bitString);
+        c = b.to_ulong();
+        // cout<<c<<endl;
+        output.write(reinterpret_cast<const char *>(&c), 1);
+        bitString.erase(0,8);
+    }
+    output.close();
 }
 
-int getNextBuffer(fstream &file)
-{   
-    clearStreams();
-    fillBuffer(file);
-    if(buffer.size()>0){
-        for (int i = 0; i < LOOKAHEADSIZE + 1 and i < buffer.size(); i++)
-        {
-            lookahead.push_back(buffer.at(i));
-            windowPointer++;
-        }
-        lookaheadPointer++;
-        dict.push_back(lookahead.at(0));
-        codigo.push_back({0, 0, lookahead.at(0)});
-        lookahead.pop_front();
-    }
-    
-    bufferCount++;
-    if(bufferCount*bufferPointer>filesize or buffer.size()==0){
-        return 0;
-    }
-    else{
-        return 1;
-    }
-}
+
 
 /*pra otimizar a windowsize basta depois definir que se o numero de bytes que eu uso pra representar <o,l,c> for maior
     do que a quantidade de bytes da string, manda a string e um identificador*/
-tuple<size_t, size_t, char> pegaMaiorSubstring()
+code getBiggestSubstring()
 {
     vector<code> substring;
-    deque<char>::iterator charIt;
-    string procura;
-    size_t posicao;
-    size_t max = 0;
-    int windowPointerBefore = windowPointer;
-    int j = 0;
-    numPop=0;
-    int i = 0;
-    int index = 0;
-    char charProcurado;
-    substring.clear();
-    procura.clear();
-    
-    charIt = find_first_of(dict.begin(), dict.end(), lookahead.begin(), lookahead.begin() + 1);
-    posicao = distance(dict.begin(), charIt);
-    if (posicao >= dict.size())
-    {   
-        lookaheadPointer++;
-        windowPointer++;
-        return make_tuple(0, 0, lookahead.at(0));
+    code sendString;
+    char a = lookahead[0];
+    size_t pos = dict.find_first_of(a);
+    size_t posaux = pos;
+    int i =0;
+    string strMatch;
+    if (pos >= dict.length()){
+        /*sem match*/
+        return {0, lookahead.substr(0,1), lookahead[0]};
+        
     }
     else{
-        /*adiciona sequencias*/
-        int k=1;
-        while(posicao<dict.size()){
-            for(int i = 0;i< lookahead.size();i++){
-                if(lookahead.size()<=1){
-                    procura += lookahead.at(i);
-                    substring.push_back({procura, dict.size() - posicao, '\0'});
-                    break;
-                }
-                else if (lookahead.at(i) == dict.at((posicao + i) % dict.size()) and i<LOOKAHEADSIZE)
-                {
-                    procura += lookahead.at(i);
-                    if (i == lookahead.size() - 1 )
-                    {
-                        if(dictPointer>=buffer.size()){
-                            substring.push_back({procura, dict.size() - posicao, '\0'});
-                            break;
-                        }
-                        if (windowPointer < buffer.size()){
-                            numPop ++;
-                            /*Pegar prox lookahead*/
-                            lookahead.push_back(buffer.at(windowPointer));
-                            windowPointer++;
-                        }
-                    }
-                }else{
-                    substring.push_back({procura, dict.size() - posicao, lookahead.at(i)});
+        while(pos!=string::npos){
+            strMatch.clear();
+            i=0;
+            while(dict[(pos+i)%dict.size()]==lookahead[i] and i<lookahead.size()){
+                strMatch+=(lookahead[i]);
+                i++;
+                a = lookahead[i];
+                if (i==lookahead.size()){
+                    a = fileBuffer[windowPointer];//ou i + 1
                     break;
                 }
             }
-            if(k>=dict.size()){
-                break;
-            }
-            charIt = find_first_of(dict.begin()+k, dict.end(), lookahead.begin(), lookahead.begin() + 1);
-            posicao = distance(dict.begin(), charIt);
-            k++;
-            procura.clear();
+            substring.push_back({dict.size() - pos, strMatch, a});
+            a = lookahead[0];
+            pos = dict.find_first_of(a, pos+1);
         }
-    }
-    
-    for(int i =0; i<numPop; i++){
-        lookahead.pop_front();
-    }
     if (substring.size() != 0)
     {
+        reverse(substring.begin(), substring.end());
+        int index = 0;
+        int max = 0;
         for (auto vec : substring)
         {
             if (vec.foundString.length() > max)
             {   
+                sendString = vec;
                 max = vec.foundString.length();
-                index++;
             }
+            index++;
         }
-        if (substring[index - 1].foundString.length() >= 1)
-        {
-            lookaheadPointer += substring[index - 1].foundString.length()+1;
-            windowPointer = lookaheadPointer+ lookahead.size();
-            return make_tuple(substring[index - 1].offset, substring[index - 1].foundString.length(), substring[index - 1].nextChar);
-        }
+            return sendString;
+    }
     }
 }
