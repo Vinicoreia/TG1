8d05027444b4d4ae667a9b4e7f7d580ae4d25edb
Vinicius Filgueiras Filho <vinicoreia@localhost.localdomain>
2018-04-17 18:43:12 -0300

    so far so good

--- izip/lz77.cpp
+++ izip/lz77.cpp
@@ -24,17 +24,17 @@ std::string getChars(int position, int len, std::string buffer){
     }
     return chars;
 }
-typedef struct Data
+struct Data
 {
     size_t offset;
-    std::string match;
+    size_t matchSize;
     char nextChar;
-
-    Data(size_t offset_, std::string match_, char nextChar_) : offset(offset_), match(match_), nextChar(nextChar_){};
-} Data;
+    Data(size_t offset, size_t matchSize, char nextChar) : offset(offset), matchSize(matchSize), nextChar(nextChar){}
+};
 
 class Dictionary{
     public:
+        std::string dictionary;
         Dictionary();
         ~Dictionary();
         int updateWindow();
@@ -48,7 +48,6 @@ class Lookahead{
     public:
         std::string lookahead;
         Lookahead(int filesize);
-        ~Lookahead();
         size_t lpb=0; /*lookahead pointer to begin of lookahead*/
         size_t lpe = 0; /*lookahead pointer to end of lookahead*/
 
@@ -57,26 +56,40 @@ class Lookahead{
 
 
 Lookahead::Lookahead(int filesize){
-    if(filesize==1){
-        exit(1);
-    }
+
+    // if(filesize<=LOOKBITS+DICTBITS+8){
+    //     /*if file size is less than the space needed to store a triple then exit*/
+    //     exit(1);
+    // }
 
     if (filesize > LOOKAHEADSIZE)
     {
         lpe += LOOKAHEADSIZE;
         lookahead.append(getChars(1, LOOKAHEADSIZE, filebuffer));
-        triplas.emplace_back(0,0,filebuffer[0]);
     }
     else
     {
         lpe += filesize;
         lookahead.append(getChars(1, filesize-1, filebuffer));
     }
-
-
+    lpb += 1;
+    triplas.emplace_back(0, 0, filebuffer[0]);
 };
 
 
 Dictionary::Dictionary(){
-
+    dpe+=1;
+    dictionary.append(getChars(0, 1, filebuffer));
 };
+
+
+
+
+int main(){
+    filebuffer = "teste";
+
+    Lookahead * look = new Lookahead(6);
+    std::cout << look->triplas.at(0).nextChar;
+    delete look;
+    return 0;
+}
