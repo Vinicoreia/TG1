3c5fa08cc6babcfb0dbf4cefdc0c4d6e10bcb699
Vinicius Filgueiras Filho <vinicoreia@localhost.localdomain>
2018-04-25 16:20:57 -0300

    problemas com caracteres especiais

--- izip/teste.cpp
+++ izip/teste.cpp
@@ -5,13 +5,16 @@
 
 int main()
 {
-    std::string corpus("hello world ello");
-    std::string pattern("llo ");
+    std::string corpus("ai você");
+    std::string pattern("você");
     std::pair<__gnu_cxx::__normal_iterator<char *, std::__cxx11::basic_string<char>>, __gnu_cxx::__normal_iterator<char *, std::__cxx11::basic_string<char>>> b = boost::algorithm::boyer_moore_search(corpus.begin(), corpus.end(),
                                                                                                                                                                                                        pattern.begin(), pattern.end());
-    std::cout << typeid(boost::algorithm::boyer_moore_search(corpus.begin(), corpus.end(),
-                                                      pattern.begin(), pattern.end())).name();
+    // std::cout << typeid(boost::algorithm::boyer_moore_search(corpus.begin(), corpus.end(),
+    //                                                   pattern.begin(), pattern.end())).name();
 
-    std::cout << "\n\n"<<corpus.size()-strlen(&b.first[0]);
+ 
+    // std::cout << "\n"<<&b.second[0];
+    char a = 'ẽ';
+    std::cout<<a;
     return 0;
 }
