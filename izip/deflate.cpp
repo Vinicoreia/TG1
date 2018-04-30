1f3ae4ecdfb722fb4cd1ee541384f54448014d81
Vinicoreia <Shadowtrolling@gmail.com>
2018-04-30 01:50:32 -0300

    adicionando pegar frequencias de uint16 e uint32 tbm

--- izip/deflate.cpp
+++ izip/deflate.cpp
@@ -1,12 +1,35 @@
 #include "deflate.h"
 #include "util.h"
+#include "lz77.h"
+#include "huff.h"
+#include <iostream>
 
-
-
-
+#define USIZE uint16_t
+#define SHIFT sizeof(USIZE)/2
+#define MASK 0xFF
 /*1- Como aplicar Burrows wheeler (antes e depois, só antes ou só depois)*/
 /*2- Como splitar, quantos blocos, a partir de qual tamanho*/
 /*3- lembrar do rle definido*/
 
 
 
+/*Chamar LZ77 ENCODE com flag encode = 1*/
+
+void DeflateEncode(std::string filenameIn, std::string filenameOut, int encode){
+    /*LZ77 PART*/
+    /*This will put the triples in the strBuffer*/
+    EncodeLZ77(filenameIn, filenameOut, 1);
+
+    std::vector<USIZE> bufferOffLen;
+    std::vector<char> bufferChar;
+    USIZE aux = 0;
+    for(int i =0; i<strBuffer.size(); i+=3){
+        aux = strBuffer[i];
+        aux = aux << SHIFT;
+        aux |= strBuffer[i+1];
+        bufferOffLen.push_back(aux);
+        bufferChar.push_back(strBuffer[i+2]);
+    }
+
+    /*Huffman Part*/
+}
