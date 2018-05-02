7d35022315f8c4e1dac739156b0bbafdb834fb1e
Vinicoreia <Shadowtrolling@gmail.com>
2018-05-02 01:30:45 -0300

    usando BWT MTF e RLE prontos

--- izip/main.cpp
+++ izip/main.cpp
@@ -2,8 +2,12 @@
 #include "deflate.h"
 #include "lz77.h"
 #include "huff.h"
+#include "BWT.h"
+#include "MTF.h"
+#include "RLE.h"
 #include <iostream>
 #include <fstream>
+#include <map>
 #include <string>
     /*
     This is a simple compressor implementation using some ideas of the folowing compressing methods:
