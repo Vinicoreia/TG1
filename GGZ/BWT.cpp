51dc1399f27d574d1fa1d894f284cf9be0640ffd
Lapschro <guiokita@gmail.com>
2018-08-12 02:28:39 -0300

    Made BWT use less memory.

--- GGZ/BWT.cpp
+++ GGZ/BWT.cpp
@@ -12,14 +12,38 @@
 
 using namespace std;
 
+vector<uint8_t> original;
+
 int SortCompare(const vector<uint8_t> &a, const vector<uint8_t> &b) {
 	/*for (int i = 0; i < a.size(); i++) {
-		if (a[i] != b[i]) {
-			return a[i] < b[i];
-		}
+	if (a[i] != b[i]) {
+	return a[i] < b[i];
+	}
 	}*/
 
 	return a < b;
+}
+
+int SortCompare1(const long long a, const long long b) {
+	vector<uint8_t> vecA, vecB;
+
+	vecA.resize(original.size());
+	vecB.resize(original.size());
+
+	memcpy(vecA.data(), original.data() + a, original.size() - a);
+	memcpy(vecA.data() + original.size() - a, original.data(), a);
+
+	memcpy(vecB.data(), original.data() + b, original.size() - b);
+	memcpy(vecB.data() + original.size() - b, original.data(), b);
+
+	return vecA < vecB;
+
+	/*for (long long i = 0; i < original.size(); i++) {
+		if (vecA[i] != vecB[i]) {
+			return vecA[i] - vecB[i];
+		}
+	}
+	return 0;*/
 }
 
 int PairCompare(pair<uint8_t, int> &a, pair<uint8_t, int> &b) {
@@ -45,16 +69,16 @@ unsigned short Find(vector<vector<uint8_t>> *vec, vector<uint8_t> *find) {
 		if (*find < vec->at(mid)) {
 			b = mid;
 		}
-		else /*if(find > vec[mid])*/{
+		else /*if(find > vec[mid])*/ {
 			a = mid;
 		}/*
-		else
-			return mid;*/
+		 else
+		 return mid;*/
 	}
 	return (unsigned short)a;
 }
 
-Data BWT::Reverse(Data toReverse, int index) {
+Data BWT::Reverse(Data toReverse, long long index) {
 	Data reverse(toReverse.size);
 
 	unordered_map<uint8_t, vector<int>> seen;
@@ -69,7 +93,7 @@ Data BWT::Reverse(Data toReverse, int index) {
 
 	sort(f.begin(), f.end());
 
-	for (int i = 0; i < toReverse.size; i++) {
+	for (long long i = 0; i < toReverse.size; i++) {
 		//Add the character to the reverse transform string
 		reverse.data[i] = f[index].first;
 		//update the index using the character at f[index] and the weight of this character. Described in Data Compression book page 155.
@@ -82,7 +106,7 @@ Data BWT::Reverse(Data toReverse, int index) {
 	return reverse;
 }
 
-void BWT::Reverse(Data * toReverse, unsigned short index)
+void BWT::Reverse(Data * toReverse, long long index)
 {
 	unordered_map<uint8_t, vector<int>> seen;
 	vector<pair<uint8_t, int>> l;
@@ -109,34 +133,44 @@ void BWT::Reverse(Data * toReverse, unsigned short index)
 	return;
 }
 
-unsigned short BWT::Transform(Data* toTransform) {
-	vector<vector<uint8_t>> rotations(toTransform->size, vector<uint8_t>(toTransform->size));
-	vector<uint8_t> original(toTransform->size);
 
-	for (int i = 0; i < toTransform->size; i++) {
+
+long long BWT::Transform(Data* toTransform) {
+	//vector<vector<uint8_t>> rotations(toTransform->size, vector<uint8_t>(toTransform->size));
+	vector<long long> rotations;
+	original.resize(toTransform->size);
+
+	for (long long i = 0; i < toTransform->size; i++) {
 		original[i] = toTransform->data[i];
+		rotations.emplace_back(i);
 	}
 
-	for (int i = 0; i < toTransform->size; i++) {
+	/*for (int i = 0; i < toTransform->size; i++) {
 		/*for (int j = 0, k = i; j < toTransform->size; j++, k++) {
-			rotations[i][j] = toTransform->data[k%toTransform->size];
-		}*/
+		rotations[i][j] = toTransform->data[k%toTransform->size];
+		}///
 		memcpy(rotations[i].data(), original.data() + i, original.size() - i);
 		memcpy((rotations[i].data() + toTransform->size - i), toTransform->data, i);
+	}*/
+
+	sort(rotations.begin(), rotations.end(), SortCompare1);
+
+	long long index;
+
+	for (long long i = 0; i < rotations.size(); i++) {
+		if (rotations[i] == 0) {
+			index = i;
+			break;
+		}
 	}
 
-	sort(rotations.begin(), rotations.end(), SortCompare);
-
-	unsigned short index;
-
-	index = Find(&rotations, &original);
-
-	for (int i = 0; i < rotations.size(); i++) {
-		toTransform->data[i] = rotations[i][rotations.size() - 1];
+	for (long long i = 0; i < rotations.size(); i++) {
+		toTransform->data[i] = original[(rotations[i] - 1 + rotations.size())%rotations.size()];
 	}
 
 	return index;
 }
+
 /*
 int main() {
 	fstream file;
