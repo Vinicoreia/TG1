7af0e10423c6df13abd19c19f725b1ae229c7403
Lapschro <guiokita@gmail.com>
2018-08-27 00:14:18 +0900

    Added adaptive arithmetic coding

---
+++ Code/AdaptiveArithmetic/Arithmetic.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "BitIO.h"
+#include <cstdint>
+
+typedef uint64_t uint64;
+
+
+class Arithmetic {
+
+	void WriteBits(bool bit) {
+		//Write bits to file, if there are pending bits, these are added as well
+		bw.WriteBit((bit) ? (uint8_t)1 : (uint8_t)0);
+		while (pendingBits > 0) {
+			bw.WriteBit((bit) ? (uint8_t)0 : (uint8_t)1);
+			pendingBits--;
+		}
+	}
+
+	uint64 precision;
+	uint64 msbMask;
+	uint64 alphabetSize;
+
+public:
+	BitWriter bw;
+	BitReader br;
+
+	Arithmetic(uint64 precision = 0xffffffffUL, uint64 msb = 0x80000000UL, uint64 alphabetSize = 256);
+
+	int pendingBits;
+
+	int ReadBit() {
+		int temp = (int)br.GetBit();
+		if (temp == -1)
+			temp = 0;
+		return temp;
+	}
+
+	void StaticEncode(fstream* output, vector<uint8_t>* input);
+	void StaticDecode(fstream* input, vector<uint8_t>* output);
+
+	void AdaptiveEncode(fstream* output, vector<uint8_t>* input);
+	void AdaptiveDecode(fstream* input, vector<uint8_t>* output);
+};
---
+++ Code/AdaptiveArithmetic/main.cpp
@@ -0,0 +1,56 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+
+#include "Arithmetic.h"
+
+
+int main(int argc, char** argv) {
+	Arithmetic art;
+	clock_t time;
+
+	std::string filename, output;
+
+	std::cin >> filename >> output;
+
+	fstream infile(filename, fstream::in | fstream::binary);
+	fstream outfile(output, fstream::out | fstream::binary);
+
+	vector<uint8_t> data;
+
+	while (1) {
+		uint8_t read;
+
+		infile.read((char*)&read, 1);
+		if (infile.eof())
+			break;
+
+		data.emplace_back(read);
+	}
+
+	time = clock();
+	art.AdaptiveEncode(&outfile, &data);
+	cout << "Time elapsed in Encoding: " << ((double)(clock() - time) / CLOCKS_PER_SEC) << endl;
+	infile.close();
+	outfile.close();
+
+	cin >> filename >> output;
+
+	infile.open(filename, fstream::in | fstream::binary);
+	outfile.open(output, fstream::out | fstream::binary);
+
+	data.clear();
+
+	art.AdaptiveDecode(&infile, &data);
+
+	outfile.write((char*)data.data(), data.size() * sizeof(uint8_t));
+
+	infile.close();
+	outfile.close();
+	
+	system("PAUSE");
+	return 0;
+}
+
