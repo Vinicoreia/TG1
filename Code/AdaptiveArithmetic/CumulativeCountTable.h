07f5a27f3fa08414db42c216d2ba5e8f6b96f003
Lapschro <guiokita@gmail.com>
2018-08-31 11:32:35 +0900

    Added results for ppm

--- Code/AdaptiveArithmetic/CumulativeCountTable.h
+++ Code/AdaptiveArithmetic/CumulativeCountTable.h
@@ -7,17 +7,18 @@ typedef uint64_t uint64;
 
 class CumulativeCountTable {
 	//maybe take out frequencies and leave only the limits
-	std::vector<long long> frequencies;
-	std::vector<long long> limits;
+	std::vector<unsigned int> frequencies;
+	std::vector<unsigned int> limits;
 
 public:
 	long long count;
 	CumulativeCountTable(std::size_t alphabetSize);
-	CumulativeCountTable(std::vector<long long> frequencyTable);
+	CumulativeCountTable(std::vector<unsigned int> frequencyTable);
+	~CumulativeCountTable();
 	char AddFrequency(std::size_t pos);
-	long long GetLow(std::size_t index);
-	long long GetHigh(std::size_t index);
-	long long GetFrequency(std::size_t index);
+	unsigned int GetLow(std::size_t index);
+	unsigned int GetHigh(std::size_t index);
+	unsigned int GetFrequency(std::size_t index);
 	int Search(long long value);
 	std::size_t Search(uint64 value);
 
